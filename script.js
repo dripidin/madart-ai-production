91fd181efbe47e2881e7f87363f078f89da34f0f
Imad Eddine <imedc5@gmail.com>
2026-04-30 05:24:43 +0100

    Update framePath to use public directory

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // ============================================
 const CONFIG = {
     totalFrames: 327, // frame_000000 to frame_000326
-    framePath: 'frames/frame_',
+    framePath: 'public/frames/frame_',
     frameExtension: '.jpg',
     preloadBatch: 10,
     scrollSpeed: 1,
